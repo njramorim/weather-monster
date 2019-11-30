d9d14cb5efe50e37cd51174390c74f5f41f8642d
Nelson Amorim Jr <namorim@uolinc.com>
2019-11-30 14:58:16 -0300

    image url https

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -1,4 +1,4 @@
 export const MAPS_HOST = 'https://www.google.com/maps/place';
 export const MAPS_URL = (lat, long) => `${MAPS_HOST}/${lat}+${long}`;
 export const WEATHER_ICON = icon =>
-  `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  `https://openweathermap.org/img/wn/${icon}@2x.png`;
