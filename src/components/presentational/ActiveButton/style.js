0cd23c997320dc30162695908351be4a11b10806
Nelson Amorim Jr <namorim@uolinc.com>
2019-12-02 11:17:50 -0300

    fixing unicode css pt2

--- src/components/presentational/ActiveButton/style.js
+++ src/components/presentational/ActiveButton/style.js
@@ -37,7 +37,7 @@ const style = component => styled(component)`
     pointer-events: none;
 
     &:before {
-      content: '️\\2714 \\fe0e';
+      content: '️\\2714';
       position: absolute;
       color: ${({ theme }) => theme.colors.highStrong};
       text-align: center;
