d745fe93f79e85239aec4ed93615f333e7806733
Nelson Amorim Jr <namorim@uolinc.com>
2019-12-02 11:52:43 -0300

    removing pseudo element check mark

--- src/components/presentational/ActiveButton/style.js
+++ src/components/presentational/ActiveButton/style.js
@@ -35,29 +35,10 @@ const style = component => styled(component)`
     border: 1px solid ${({ theme }) => theme.colors.light};
     transition: background-color 0.3s;
     pointer-events: none;
-
-    &:before {
-      content: '️\\2714';
-      position: absolute;
-      color: ${({ theme }) => theme.colors.highStrong};
-      text-align: center;
-      font-size: 2.5rem;
-      border-radius: inherit;
-      transform-origin: center center;
-      transform: scale(0, 0.3);
-      transition: all 0.2s ease-in 0.1s;
-      width: 100%;
-      height: 100%;
-      opacity: 0.8;
-    }
   }
 
   .hideField:checked + .inputStyle {
     background-color: ${({ theme }) => theme.colors.light};
-
-    &:before {
-      transform: scale(1);
-    }
   }
 `;
 
